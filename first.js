abdb955a5c1e5b2f0a9637fb7ae7da4cf4c685be
mohangpt <59196576+mohangpt@users.noreply.github.com>
2020-12-28 20:20:52 +0530

    Update first.js

--- first.js
+++ first.js
@@ -1,5 +1,5 @@
 (function (shop) { 
    var div = document.getElementById("scriptApp");
-   div.innerHTML += <div> <p>this is a box</p></div>
+   div.innerHTML += '<div> <p>this is a box</p></div>'
    console.log('I am from script tag for shop ' + shop); 
 })(Shopify.shop)
