820d992671eb54c9f17804c7bd1937891b172cc7
mohangpt <59196576+mohangpt@users.noreply.github.com>
2020-12-28 20:17:24 +0530

    Update first.js

--- first.js
+++ first.js
@@ -1,4 +1,5 @@
 (function (shop) { 
-   alert(`Hello ${shop}`); 
+   var div = document.getElementById("scriptApp");
+   div.innerHTML += <div> <p>this is a box</p></div>
    console.log('I am from script tag for shop ' + shop); 
 })(Shopify.shop)
