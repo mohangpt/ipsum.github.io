6f18912d0a70461967984e2eef9ad1cfae1cba11
mohangpt <59196576+mohangpt@users.noreply.github.com>
2021-03-10 00:11:03 +0530

    Update first.js
    adding select

--- first.js
+++ first.js
@@ -1,5 +1,10 @@
 (function (shop) { 
    var div = document.getElementById("scriptApp");
-   div.innerHTML += '<div> <p>this is a box</p></div>'
+   div.innerHTML += '<div>\
+      <select name="cars" id="cars">\
+      <option value="volvo">Left Eye</option>\
+      <option value="saab">Right Eye</option>\
+      </select>\
+      </div>'
    console.log('I am from script tag for shop ' + shop); 
 })(Shopify.shop)
